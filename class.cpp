adf9514ec0876d9fcbf1a466d6391765db3166e3
ronaldroldanurrutia <ajedrez_11@hotmail.com>
2016-11-08 06:18:08 -0500

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<set>
+#include <algorithm>
+
+
+using namespace std;
+
+template <typename T>
+class Point{
+    private:
+        T x;
+        T y;
+    public:
+        Point(T _x,T _y){
+            this->x=_x;
+            this->y=_y;
+        }
+        T getx(){
+            return this->x;
+        }
+        T gety(){
+            return this->y;
+        }
+
+};
+
+int main(){
+    set<Point <int> >ins;
+
+    ins.insert(Point<int> w(1,2));
+
+}
